26dbc6a4d6fd48fe4955ed34887049226ad56738
Florian Giambiasi <fgmb@pm.me>
2020-11-04 16:39:23 +0100

    my-first-react-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Member from './components/Member'
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <Member name='Flo'/>
+      <Member name='Samah'/>
+      <Member name='Lol'/>
+      <Member name='Lil'/>
     </div>
   );
 }
---
+++ src/components/Member.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Member = ({name}) => {
+    return (
+        <h2>Member of my family : {name.toUpperCase()}</h2>
+        
+    )
+}
+
+export default Member
+
+
